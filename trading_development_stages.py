fde1a029bb16d435b51017912811593300545cee
Victor-Maj <202206971@post.au.dk>
2025-07-04 16:10:47 +0200

    update read.me

---
+++ trading_development_stages.py
@@ -0,0 +1,10 @@
+"""
+This file will mainly comprices of 4 stages do determine if a strategy is profitable.
+
+Stage 1: In-sample Excellence
+Stage 2: In-sample Permuation test
+Stage 3: Walk Forward Test
+Stage 4: Walk Forward Permuation Test
+
+This as 
+"""
